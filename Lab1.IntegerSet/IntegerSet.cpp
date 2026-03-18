45bdf1ae75013edb7ed6eab4200c9426c67c3c6f
Tangchangyu <t2467352616@Gmail.com>
2026-03-18 14:04:45 +0800

    Using validEntry to check the array index;初始化列表 using 初始化列表 to initialize the object————the array of bools;

--- Lab1.IntegerSet/IntegerSet.cpp
+++ Lab1.IntegerSet/IntegerSet.cpp
@@ -1,12 +1,18 @@
 #include"IntegerSet.h"
 #include<stdexcept>
 #include<iostream>
+bool validEntry(int a){
+    if (a >=0 && a <101) return 1;
+    else throw std::invalid_argument("Not a valid integer!");
+    return 0;
+
+}
 
 IntegerSet::IntegerSet():set{}{};//// set{} 会把数组所有元素初始化为该类型的默认值（bool 就是 false）
 IntegerSet::IntegerSet(const int* a,const int & lenth){
     if (lenth <= 101){
         for (int i = 0; i < lenth;i++){
-            set[a[i]] = 1;
+            if(validEntry(a[i])) set[a[i]] = 1;
         }
     }//数值范围检查，防止数组访问越界；
     else throw std::invalid_argument("too big array, the maximum of array size is 101");
@@ -32,15 +38,19 @@ IntegerSet IntegerSet::intersectionOfSets(const IntegerSet& a ,const IntegerSet&
 }
 
 void IntegerSet::insertElement(int k){
-    if (0<=k && k<101){
+    if (validEntry(k)){
         set[k]=1;
     }
 
-    else throw std::invalid_argument("Invalid integer(0-100)");
-
 }
 
-void IntegerSet::printSet(){
+void IntegerSet::deleteElement(int k){
+    if (validEntry(k)){
+        set[k]=0;
+    }
+}
+
+void IntegerSet::printSet()const{
     using std::cout;
     bool flag = 0;
     for (int i = 0;i<101;i++){
@@ -49,7 +59,20 @@ void IntegerSet::printSet(){
             flag = 1;
         }
     }
-    if (flag == 1){
+    if (flag == 0){
         cout<<"___";
     }
 }
+
+bool IntegerSet::isEqualto(const IntegerSet& other)const{
+    bool flag = 1;
+    for (int i = 0;i <101; i++){
+        if (set[i]!= other.set[i]){
+            flag = 0;
+            break;
+        }
+    }
+    return flag;
+}
+
+
---
+++ Lab1.IntegerSet/IntegerSet.h
@@ -0,0 +1,17 @@
+#pragma once
+
+class IntegerSet{
+    private:
+    bool set [101]={0};//为什么要在构造函数初始化？
+
+public:
+    IntegerSet();//初始化列表会将所有元素初始化为默认值，bool———false;
+    IntegerSet(const int *, const int &);
+
+    IntegerSet unionOfSets(const IntegerSet&,const IntegerSet&)const;
+    IntegerSet intersectionOfSets(const IntegerSet&,const IntegerSet&)const;
+    void insertElement(int k);
+    void deleteElement(int k);
+    void printSet()const;
+    bool isEqualto(const IntegerSet&)const;
+};

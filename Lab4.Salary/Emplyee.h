410d2b536901e8b7806826420cd21262b49c19c9
Tangchangyu <t2467352616@Gmail.com>
2026-05-09 10:18:05 +0800

    修改print函数的细节

--- Lab4.Salary/Emplyee.h
+++ Lab4.Salary/Emplyee.h
@@ -2,16 +2,19 @@
 
 #include<iostream>
 #include<string>
+#include<stdexcept>
 using std::string;
+using std::cout;
 
 class Employee{
 private:
     string firstname;
     string lastname;
     string socialSecurityNumber;
+    string birsday;
 public:
 
-    Employee(const string& fn,const string &ln,const string& ssn ):firstname(fn),lastname(ln),socialSecurityNumber(ssn){
+    Employee(const string& fn,const string &ln,const string& ssn ,const string& bir):firstname(fn),lastname(ln),socialSecurityNumber(ssn),birsday(bir){
 
     }
 
@@ -39,9 +42,131 @@ public:
         return socialSecurityNumber;
     }
 
-    void print()const{
+    virtual void print()const{
         using std::cout;
         cout<<getFirstname()<<" "<<getLastname()<<std::endl<<"social secirity number: "<<getSocialSecurityNumber();
     }
     
+    virtual double earnings()const = 0;
+
 };
+
+class SalariedEmployee : public Employee{
+private:
+    double weeklySalary;
+    void check() {
+         if (weeklySalary <= 0 ){
+            throw std::invalid_argument("salary must be >= 0.");
+        }
+    }
+public:
+    SalariedEmployee(const string& fn ,const string& ln, const string& ssn,const string &bir,double s = 0.0 ):Employee(fn,ln, ssn,bir),weeklySalary(s){
+       check();
+    };
+
+    virtual ~SalariedEmployee(){};
+
+    void setWeeklySalary(double s ){
+        weeklySalary = s;
+    }
+
+    double getWeeklySalary()const{
+        return weeklySalary;
+    }
+
+    virtual double earnings ()const override{return getWeeklySalary();}
+
+    virtual void print() const override{
+        std::cout<<"salaried employed:";
+        Employee::print();
+        std::cout <<"\nweekly salary: "<<getWeeklySalary();
+    }
+};
+
+class CommissionEmployee: public Employee{
+private:
+    double grossSales;
+    double commissionRate;
+public:
+    CommissionEmployee(const string& fn,const string &ln,const string& ssn,const string &bir,double a = 0.0,double b = 0.0):Employee(fn,ln, ssn,bir){
+        setComissionrate(b);
+        setGrossSales(a);
+    }
+
+    void setGrossSales(double s){
+        if (s < 0 ){
+            throw std::invalid_argument("gross sales must be >= 0 ");
+        }
+        else grossSales = s;
+    }
+
+    double getGrossSales()const{
+        return grossSales;
+    }
+
+    void setComissionrate(double s){
+        if (s < 0 ){
+            throw std::invalid_argument("commission rate must be >= 0 ");
+        }
+        else commissionRate = s;
+    }
+
+    double getCommissionrate()const{
+        return commissionRate;
+    }
+
+    virtual double earnings()const override{
+        return getCommissionrate()*getGrossSales();
+    }
+
+    virtual void print ()const override{
+        std::cout <<"commission employee: ";
+        Employee::print();
+        std::cout<< "\ngross sales: "<<getGrossSales()<<"; commission rate: "<<getCommissionrate();
+
+    }
+
+};
+
+class hourlyEmployee: public Employee{
+private:
+    double hourlyWage;
+    double hoursWorked;
+public:
+    hourlyEmployee(const string & fn,const string & ln,const string &ssn,const string &b,double hm=0.0,double h = 0.0):Employee(fn,ln,ssn,b){
+        setHourlyWage(hm);
+        setHoursWorked(h);
+    }
+
+    void setHourlyWage(double a ){
+        if (a<=0){
+            throw std::invalid_argument("hourly wage mustbe >= 0");
+        }
+        else hourlyWage = a;
+    }
+     void setHoursWorked(double a ){
+         if (a<=0){
+            throw std::invalid_argument("work hour mustbe >= 0");
+        }
+        else hoursWorked = a;
+    }
+
+    double getHourlyWage()const{
+        return hourlyWage;
+    }
+
+    double getHoursWored()const{
+        return hoursWorked;
+    }
+
+    virtual double earnings()const override{
+        return hourlyWage*hoursWorked;
+    }
+
+    virtual void print()const override{
+        std::cout <<"\nhourly employee: ";
+        Employee::print();
+        cout<<"hourly wage: "<<getHourlyWage()<<"; hours worked: "<<getHoursWored()<<"\n"
+    }
+
+}

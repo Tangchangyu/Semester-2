bfd0a26bf70715dc372e7380b74d1cd9ef7da5e1
Tangchangyu <t2467352616@Gmail.com>
2026-05-26 21:02:48 +0800

    what()函数本身只抛出字符串，不会输出到屏幕上。

--- Lab6.reThrow/reThrowException.cpp
+++ Lab6.reThrow/reThrowException.cpp
@@ -10,7 +10,6 @@ using std::cout;
 class TestException: public runtime_error{
     public:
     TestException(const string &s):runtime_error(s){
-        cout<<"catch an exception."<<std::endl;
     }
 
 
@@ -21,8 +20,19 @@ void g(){
     try{
         throw TestException("an TestException.");
     }
-    catch (const TestException& ex){
-        ex.what();
-        throw ex;
+    catch (...){
+        cout<<"Exception caught in function g(). Rethrowing... \n";
+        throw ;
+    }
+}
+
+int main(){
+    try{
+        g();
+    }
+
+    catch (const runtime_error& ex){
+        cout<<"Excetion caught in function main()\n";
+        cout<<ex.what();
     }
 }

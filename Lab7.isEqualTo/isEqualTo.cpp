c0ced346b91b2008042650f5cf2881ee731ecb8e
Tangchangyu <t2467352616@Gmail.com>
2026-06-02 23:01:59 +0800

    done

--- Lab7.isEqualTo/isEqualTo.cpp
+++ Lab7.isEqualTo/isEqualTo.cpp
@@ -11,8 +11,10 @@ bool isEqualTo(T a,T b){
 template<class T>
 void print(T a,T b){
     cout<<a<<(isEqualTo(a,b)?" is":" is not ")<<"equal to "<<b<<std::endl;
-}
+}//封装输出逻辑；
 
+/*
+//常见类型的isEqualTo调用；
 int main(){
     int a=0, b = 1;
     double c =1.8;
@@ -24,4 +26,13 @@ int main(){
     print (d,'n');
     print(f,true);
 
+}*/
+
+
+//main for Complex test
+int main(){
+    cout<<isEqualTo(Complex(1,1),Complex(1,1));
 }
+    //若未定义==，报错：undefined reference to `Complex::operator==(Complex const&) const'
+
+
